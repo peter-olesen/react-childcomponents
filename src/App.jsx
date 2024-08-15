1895d2b72f2749fa934c85a603cb75510384279c
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-08-15 13:59:52 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { Wrapper } from './components/Wrapper/Wrapper'
+import { Card, CardHeader, CardContent, CardFooter } from './components/Card/Card'
 import './App.scss'
 
 function App() {
 
   return (
     <>
+    
     <Wrapper
-      title="Hello"
+      title = "Hello"
       subTitle = "There"
       >
       <h1>Hello again</h1>
     </Wrapper>
+
+    <Card>
+        <CardHeader CardTitle = "Fack me" />
+        <CardContent CardText = "Lorem ipsum dolor sit amet" />
+        <CardFooter CardCTA = "Wooop" />
+    </Card>
+
+    <Card>
+      <CardHeader />
+      <CardContent />
+      <CardFooter />
+    </Card>
+
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import style from "./Card.module.scss"
+
+export const Card = ({ children }) => {
+
+    return (
+        <div className={style.Card}>
+            {children}
+        </div>
+    )
+}
+
+export const CardHeader = ({ CardTitle = "Default Header" }) => {
+    return (
+        <h2>{CardTitle}</h2>
+    )
+}
+
+export const CardContent = ({ CardText = "Default Content" }) => {
+    return (
+        <p>{CardText}</p>
+    )
+}
+
+export const CardFooter = ({ CardCTA = "Default Footer Text" }) => {
+    return (
+        <h2>{CardCTA}</h2>
+    )
+}
